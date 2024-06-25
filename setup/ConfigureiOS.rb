4d2151a0097385631cb86778e2c861727c63bb42
zylll520 <zylll527@gmail.com>
2024-06-25 18:15:55 +0800

    reset config

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -13,9 +13,9 @@ module Pod
 
     def perform
 
-      keep_demo = :no
+      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
 
-      framework = :none
+      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
       case framework
         when :specta
           configurator.add_pod_to_podfile "Specta"
@@ -35,7 +35,7 @@ module Pod
           configurator.set_test_framework("xctest", "m", "ios")
       end
 
-      snapshots = :no
+      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
       case snapshots
         when :yes
           configurator.add_pod_to_podfile "FBSnapshotTestCase"
